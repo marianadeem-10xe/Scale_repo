58fc94300145716c7876a3deaaca861501a9ca5e
Maria_Nadeem-10x <maria.nadeem@10xengineers.ai>
2022-11-01 12:46:58 +0500

    - Generated results on Graph images.

--- Main_scale.py
+++ Main_scale.py
@@ -4,13 +4,16 @@ from utils_scale import Scale
 from matplotlib import pyplot as plt
 
 
-raw_path        = "./text_img_2592x1944.jpg"
+raw_path        = "./results/Graph images/ColorDifferenceError_20221026_131952.png"
 size            = (1944, 2592, 3)          # (height, width)
-scale_to_size   = (480, 640, 3)
-output_filename = "./Downscaled_text_{}x{}.jpg".format(scale_to_size[0], scale_to_size[1]) 
+scale_to_size   = (1440, 2560, 3)
+output_filename = "./Downscaled_ColorDifferenceError_{}x{}.jpg".format(scale_to_size[0], scale_to_size[1]) 
 
 raw_file = cv2.imread(raw_path)
 raw_file = cv2.cvtColor(raw_file, cv2.COLOR_BGR2RGB)
+
+raw_file = cv2.resize(raw_file, (2592, 1944), interpolation= cv2.INTER_LINEAR)
+# plt.imsave("./Input images/Density2_2592x1944.png", raw_file)
 
 assert raw_file.shape==size, "Input size must be 2592x1944!"
 
